8d30662e85016613c263bdd5fa0fe2be0544eea0
Seiji Matsumoto <seijim27@gmail.com>
2024-02-10 11:26:34 -0500

    Simplify test

--- __tests__/Main.test.tsx
+++ __tests__/Main.test.tsx
@@ -1,34 +1,12 @@
-/* eslint-disable testing-library/prefer-screen-queries */
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Main from '../src/components/Main/Main';
-
-jest.mock('ai/react', () => ({
-  useCompletion: jest.fn(() => ({
-    complete: jest.fn(),
-    isLoading: false,
-    message: 'mock',
-    stop: jest.fn(),
-  })),
-}));
 
 describe('Main component', () => {
   it('renders without crashing', () => {
-    render(<Main />);
-  });
-
-  it('submits the form correctly', () => {
     const { getByTestId } = render(<Main />);
-    const form = getByTestId('main-form');
-    fireEvent.submit(form);
+    const mainParentComponent = getByTestId('main-parent-component');
+    expect(mainParentComponent).toBeInTheDocument();
+    expect(mainParentComponent.children.length).toBe(2);
   });
-
 });
-
-/*
-  Tests to add
-  - form is submitted with only 1 input field filled out
-  - form is submitted with no input fields filled out
-  - on load, message should be empty
-  - after submitHandler is invoked, message should no longer be empty
-*/
 
