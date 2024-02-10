32255f5a21be2a79506f43d7c86e0f76fbcae98f
Seiji Matsumoto <seijim27@gmail.com>
2024-02-10 13:54:04 -0500

    cypress init

---
+++ cypress/e2e/main.cy.ts
@@ -0,0 +1,22 @@
+import { aboutMe, jobDescription } from "../../data/dummy";
+
+describe("PrepareWithAi", () => {
+  it("successfully loads", () => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it("generates data", () => {
+    cy.visit("http://localhost:3000");
+    cy.get('[data-testid="textarea1"]').as("textarea1");
+    cy.get('[data-testid="textarea2"]').as("textarea2");
+
+    cy.get("@textarea1").type(aboutMe);
+    cy.get("@textarea1").should("have.value", aboutMe);
+
+    cy.get("@textarea2").type(jobDescription);
+    cy.get("@textarea2").should("have.value", jobDescription);
+
+    cy.get('[data-testid="submit-btn"]').click();
+    cy.get('[data-testid="loading-indicator"]').should("exist");
+  });
+});
